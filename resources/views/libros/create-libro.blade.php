a7c163832a957e648576c272e85efc05eb87c894
Angel Aguayo <jose.aguayo4679@alumnos.udg.mx>
2024-11-21 20:28:55 -0600

    Agregar modelo genero

--- resources/views/libros/create-libro.blade.php
+++ resources/views/libros/create-libro.blade.php
@@ -14,7 +14,7 @@
             <div class="row mb-3">
                 <label class="col-sm-2 col-form-label" for="titulo">Titulo</label>
                 <div class="col-sm-10">
-                <input type="text" class="form-control" name="titulo" id="titulo" value="{{ old('titulo') }}">
+                <input type="text" class="form-control" name="titulo" id="titulo" value="{{ old('titulo') }}" placeholder="Ingrese el título del libro.">
                 @error('titulo')
                     <div class="alert alert-danger" role="alert">{{ $message }}</div>
                 @enderror
@@ -24,7 +24,7 @@
             <div class="row mb-3">
                 <label class="col-sm-2 col-form-label" for="autor">Autor</label>
                 <div class="col-sm-10">
-                <input type="text" class="form-control" name="autor" id="autor" value="{{ old('autor') }}">
+                <input type="text" class="form-control" name="autor" id="autor" value="{{ old('autor') }}" placeholder="Ingrese el nombre del autor.">
                 @error('autor')
                     <div class="alert alert-danger" role="alert">{{ $message }}</div>
                 @enderror
@@ -34,7 +34,7 @@
             <div class="row mb-3">
                 <label class="col-sm-2 col-form-label" for="editorial">Editorial</label>
                 <div class="col-sm-10">
-                <input type="text" class="form-control" name="editorial" id="editorial" value="{{ old('editorial') }}">
+                <input type="text" class="form-control" name="editorial" id="editorial" value="{{ old('editorial') }}" placeholder="Ingrese la editorial (opcional).">
                 @error('editorial')
                     <div class="alert alert-danger" role="alert">{{ $message }}</div>
                 @enderror
@@ -44,7 +44,7 @@
             <div class="row mb-3">
                 <label class="col-sm-2 col-form-label" for="editorial">Precio</label>
                 <div class="col-sm-10">
-                <input type="text" class="form-control" name="precio" id="precio" value="{{ old('precio') }}">
+                <input type="text" class="form-control" name="precio" id="precio" value="{{ old('precio') }}" placeholder="Ingrese el precio (ej. 199.99).">
                 @error('precio')
                     <div class="alert alert-danger" role="alert">{{ $message }}</div>
                 @enderror
@@ -54,7 +54,7 @@
             <div class="row mb-3">
                 <label class="col-sm-2 col-form-label" for="anio_publicacion">Año de Publicación</label>
                 <div class="col-sm-10">
-                <input type="number" class="form-control" name="anio_publicacion" id="anio_publicacion" value="{{ old('anio_publicacion') }}">
+                <input type="number" class="form-control" name="anio_publicacion" id="anio_publicacion" value="{{ old('anio_publicacion') }}" placeholder="Ingrese el año de publicación (ej. 2023).">
                 @error('anio_publicacion')
                     <div class="alert alert-danger" role="alert">{{ $message }}</div>
                 @enderror
@@ -64,7 +64,7 @@
             <div class="row mb-3">
                 <label class="col-sm-2 col-form-label" for="isbn">ISBN</label>
                 <div class="col-sm-10">
-                <input type="text" class="form-control" name="isbn" id="isbn" value="{{ old('isbn') }}">
+                <input type="text" class="form-control" name="isbn" id="isbn" value="{{ old('isbn') }}" placeholder="Ingrese el ISBN único del libro.">
                 @error('isbn')
                     <div class="alert alert-danger" role="alert">{{ $message }}</div>
                 @enderror
@@ -74,7 +74,7 @@
             <div class="row mb-3">
                 <label class="col-sm-2 col-form-label" for="paginas">Número de Páginas</label>
                 <div class="col-sm-10">
-                <input type="number" class="form-control" name="paginas" id="paginas" value="{{ old('paginas') }}">
+                <input type="number" class="form-control" name="paginas" id="paginas" value="{{ old('paginas') }}" placeholder="Ingrese el número de páginas.">
                 @error('paginas')
                     <div class="alert alert-danger" role="alert">{{ $message }}</div>
                 @enderror
@@ -101,7 +101,7 @@
             <div class="row mb-3">
                 <label class="col-sm-2 col-form-label" for="sinopsis">Sinopsis</label>
                 <div class="col-sm-10">
-                <textarea name="sinopsis" class="form-control" id="sinopsis" cols="30" rows="4">{{ old('sinopsis') }}</textarea>
+                <textarea name="sinopsis" class="form-control" id="sinopsis" cols="30" rows="4" placeholder="Ingrese una breve sinopsis del libro (opcional).">{{ old('sinopsis') }}</textarea>
                 @error('sinopsis')
                     <div class="alert alert-danger" role="alert">{{ $message }}</div>
                 @enderror
